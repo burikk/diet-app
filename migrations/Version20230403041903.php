ab0311f9a0931038dc4c944297ba41e6a224b9f1
Oleksandr Burkotenko <burkotenko.s@gmail.com>
2023-04-03 06:25:38 +0200

    add diet entity

---
+++ migrations/Version20230403041903.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230403041903 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE diet (id INT AUTO_INCREMENT NOT NULL, country_code VARCHAR(2) NOT NULL, total_days INT NOT NULL, amount INT NOT NULL, date_start DATETIME NOT NULL, date_end DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE diet');
+    }
+}
---
+++ src/Entity/Diet.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\DietRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DietRepository::class)]
+class Diet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 2)]
+    private string $countryCode;
+
+    #[ORM\Column]
+    private int $totalDays;
+
+    #[ORM\Column]
+    private int $amount;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private \DateTime $dateStart;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private \DateTime $dateEnd;
+
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getCountryCode(): string
+    {
+        return $this->countryCode;
+    }
+
+    public function setCountryCode(string $countryCode): self
+    {
+        $this->countryCode = $countryCode;
+
+        return $this;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getTotalDays(): int
+    {
+        return $this->totalDays;
+    }
+
+    public function setTotalDays(int $totalDays): self
+    {
+        $this->totalDays = $totalDays;
+
+        return $this;
+    }
+
+    public function getDateStart(): \DateTime
+    {
+        return $this->dateStart;
+    }
+
+    public function setDateStart(\DateTime $dateStart): self
+    {
+        $this->dateStart = $dateStart;
+
+        return $this;
+    }
+
+    public function getDateEnd(): \DateTime
+    {
+        return $this->dateEnd;
+    }
+
+    public function setDateEnd(\DateTime $dateEnd): self
+    {
+        $this->dateEnd = $dateEnd;
+
+        return $this;
+    }
+}
